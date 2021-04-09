31872e61ea8579d3ef100815dbaec5b62c825f40
Min Chang <minchangmhc@gmail.com>
2021-04-09 16:14:18 -0700

    WIP: adding objects into orders attribute.

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,43 @@
 // -----Utility Logic-----
 
 // -----Business Logic-----
-function Orders() {
-  this.pizzaPrice = {};
-  this.price = 0;
-  this.currentId = 0;
-}
-
-Orders.prototype.assignId = function () {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-Orders.prototype.addPrice = function (cost) {
-  this.pizzaPrice[cost] = cost
-}
-
-Orders.prototype.smallPrice = function () {
-  this.price += 10;
-  return this.price;
-}
-
-Orders.prototype.mediumPrice = function () {
-  this.price += 10;
-  return this.price;
-}
-
-Orders.prototype.largePrice = function () {
-  this.price += 10;
-  return this.price;
-}
-function Pizza(size, toppings, price) {
+function Pizza(size, toppings) {
+  this.orders = {};
   this.size = size;
   this.toppings = toppings;
-  this.price = price;
+  this.price = 0;
 }
 
-Pizza.prototype.entireOrder = function () {
-  return this.size + " " + this.price;
+Pizza.prototype.addPrice = function (cost) {
+  return this.orders = cost
 }
 
+Pizza.prototype.smallPrice = function () {
+  this.price += 10;
+  return this.price;
+}
 
+Pizza.prototype.mediumPrice = function () {
+  this.price += 10;
+  return this.price;
+}
+
+Pizza.prototype.largePrice = function () {
+  this.price += 10;
+  return this.price;
+}
+
+function CalculatePrice(small, medium, large, topping) {
+  this.small = small
+  this.medium = medium
+  this.large = large
+  this.topping = topping
+}
+
+CalculatePrice.prototype.small = function (small) {
+  const price = 6
+  return small = price
+}
 
 
 // -----User Interface Logic-----
-// function displayPrice(priceToDisplay) {
-//   let results = $("#total");
-//   let htmlForPizzaPrice = "";
-//   Object.keys(priceToDisplay.pizzaPrice).forEach(function (key) {
-//     const pizzaCost = priceToDisplay.addPrice(key)
-//     htmlForPizzaPrice = pizzaCost.price
-//   })
-//   results.html(htmlForPizzaPrice)
-// }
-
-$(document).ready(function () {
-  let newPizza = new Pizza();
-  $("form#form").submit(function (event) {
-    event.preventDefault();
-    const small = $("input:radio[id=small]:checked").val();
-    const medium = $("input:radio[id=medium]:checked").val();
-    const large = $("input:radio[id=large]:checked").val();
-    let price;
-    if (small === "smallSize") {
-      newPizza.addPrice();
-    } else if (medium === "mediumSize") {
-      newPizza.addPrice();
-    } else if (large === "largeSize") {
-      newPizza.addPrice();
-    } else {
-
-    }
-  })
-  // newPizza.addPrice();
-  // displayPrice(newPizza);
-})
