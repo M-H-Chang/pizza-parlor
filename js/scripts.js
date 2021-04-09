3e9b0bc3f60cdd9f2561ce81257ff6082436f271
Min Chang <minchangmhc@gmail.com>
2021-04-09 10:59:10 -0700

    WIP: price prototype method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 // -----Utility Logic-----
 
 // -----Business Logic-----
-function Pizza(size, toppings, price) {
+function Pizza(size, toppings) {
   this.size = size
   this.toppings = toppings
+  this.price = price
 }
 
 Pizza.prototype.price = function (cost) {
-  this["cost"] = cost
+  return this["price"] = cost
 }
+
+function CalculatePrice(small, medium, large, topping) {
+  this.small = small
+  this.medium = medium
+  this.large = large
+  this.topping = topping
+}
+
+CalculatePrice.prototype.small = function (small) {
+  const price = 6
+  return small = price
+}
+
+
 // -----User Interface Logic-----
