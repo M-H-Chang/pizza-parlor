af620f39a3d895b33b4993b1f6d43b78d8e98055
Min Chang <minchangmhc@gmail.com>
2021-04-10 11:54:57 -0700

    BUGFIX: the price was not displaying in the console.

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,29 @@ function Pizza(size, toppings) {
   this.price = 0;
 }
 
-Pizza.prototype.addPrice = function (smallCost) {
-  return this.orders = smallCost
+Pizza.prototype.addPrice = function (price) {
+  this.orders[price] = price;
 }
 
-Pizza.prototype.smallPrice = function () {
-  this.price += 10;
-  return this.price;
-}
+// Pizza.prototype.smallPrice = function () {
+//   this.price += 10;
+//   return this.price;
+// }
 
-Pizza.prototype.mediumPrice = function () {
-  this.price += 10;
-  return this.price;
-}
+// Pizza.prototype.mediumPrice = function () {
+//   this.price += 10;
+//   return this.price;
+// }
 
-Pizza.prototype.largePrice = function () {
-  this.price += 10;
-  return this.price;
-}
+// Pizza.prototype.largePrice = function () {
+//   this.price += 10;
+//   return this.price;
+// }
 
-function CalculatePrice(small, medium, large, topping) {
-  this.small = small
-  this.medium = medium
-  this.large = large
-  this.topping = topping
-}
+// function Order(size, topping) {
+//   this.size = size;
+//   this.topping = topping;
+// }  // use this for the display price function
 
 CalculatePrice.prototype.small = function (small) {
   const price = 6
@@ -41,3 +39,23 @@ CalculatePrice.prototype.small = function (small) {
 
 
 // -----User Interface Logic-----
+function displayPrice(priceToDisplay) {
+  let priceList = $("#total");
+  let htmlForPrice = "";
+  Object.keys(priceToDisplay.orders).forEach(function (key) {
+    const newPrice = priceToDisplay.addPrice(key);
+    htmlForPrice += "<li>" + newPrice.size + " " + newPrice.topping + "</li>";
+  })
+  priceList.html(htmlForPrice);
+}
+
+$(documnet).ready(function () {
+  $("#form").submit(function (event) {
+    event.preventDefault();
+    const small
+    const medium
+    const largePrice
+
+
+  })
+})
